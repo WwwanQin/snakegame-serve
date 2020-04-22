28cbc222d0d4c23339d826072a020ffc810a45e4
yusen <xm36447864@126.com>
2020-04-22 23:51:21 +0800

    对传过来的状态进行了相应的事件处理

--- controller/ws_controller.js
+++ controller/ws_controller.js
@@ -1,73 +1,113 @@
 const ws = require('nodejs-websocket')
 var wsserver;
-const xAtoms = 40;
-const yAtoms = 30;
+const xAtoms = 20;
+const yAtoms = 15;
 const height = 20;
 const width = 20;
+// 客户端ID
+const startQueue = new Set();
+// 食物触发的次数
+let foodIndex = 0;
+// 处理所有事件的集中的对象数组
+const bundleFunction = [
+    // 等待所有玩家连接
+    {
+        type: 'gameStart',
+        fn: (sendMessage,callBackFunction) => {
+            startQueue.add(sendMessage.id);
+            if(startQueue.size == 1){
+                callBackFunction(wsserver,JSON.stringify(
+                        {
+                            type:'waiting',
+                            message:`等待连接中，目前连接数：${startQueue.size} / 2`,
+                        }
+                    )
+                ); 
+            }
+            if(startQueue.size == 2){
+                const xDistance = Math.floor(Math.random() * xAtoms);
+                const yDistance = Math.floor(Math.random() * yAtoms);
+                callBackFunction(wsserver,JSON.stringify(
+                        {
+                            type:'startGame',
+                            message:`已连接到游戏，目前连接数：${startQueue.size} / 2`,
+                            xDistance: xDistance * width,
+                            yDistance: yDistance * height
+                        }
+                    )
+                );
+                startQueue.clear();
+            }
+        }
+    },
+    // 获取蛇运动的方位
+    {
+        type: 'snakeRun',
+        fn: (sendMessage,callBackFunction) => {
+            console.log(`广播蛇的方位${sendMessage.position}`);
+            callBackFunction(wsserver,JSON.stringify(
+                    {
+                        type:'snakeRun',
+                        message: sendMessage.position,
+                    }
+                )
+            );
+        }
+    },
+    // 判断是否吃到了食物
+    {
+        type: 'eatFood',
+        fn: (sendMessage,callBackFunction) => {
+            foodIndex ++ ;
+            if(foodIndex == 1){
+                const xDistance = Math.floor(Math.random() * xAtoms);
+                const yDistance = Math.floor(Math.random() * yAtoms);
+                callBackFunction(wsserver,JSON.stringify(
+                        {
+                            type:'eatFood',
+                            xDistance: xDistance * width,
+                            yDistance: yDistance * height
+                        }
+                    )
+                );
+            }
+            if(foodIndex == 2){
+                foodIndex = 0;
+            }
+        }
+    },
+    // 结束暂停开始游戏
+    {
+        type: 'start',
+        fn: (sendMessage,callBackFunction) => {
+            callBackFunction(wsserver,JSON.stringify(
+                {
+                    type: 'start'
+                }
+            ));
+        }
+    },
+    // 暂停游戏
+    {
+        type: 'pause',
+        fn: (sendMessage,callBackFunction) => {
+            callBackFunction(wsserver,JSON.stringify(
+                {
+                    type: 'pause'
+                }
+            ))
+        }
+    }
+]
 class wbServe{
     constructor(port){
         this.port = port   
-        const startQueue = new Set();
-        let foodIndex = 0;
         wsserver = ws.createServer(conn => {
             conn.on('text',str => {
                 let sendMessage = JSON.parse(str);
-                // 等待所有玩家连接
-                if(sendMessage.message == 'gameStart'){
-                    startQueue.add(sendMessage.id);
-                    if(startQueue.size == 1){
-                        broadcast(wsserver,JSON.stringify(
-                                {
-                                    type:'waiting',
-                                    message:`等待连接中，目前连接数：${startQueue.size} / 2`,
-                                }
-                            )
-                        ); 
-                    }
-                    if(startQueue.size == 2){
-                        const xDistance = Math.floor(Math.random() * xAtoms);
-                        const yDistance = Math.floor(Math.random() * yAtoms);
-                        broadcast(wsserver,JSON.stringify(
-                                {
-                                    type:'startGame',
-                                    message:`已连接到游戏，目前连接数：${startQueue.size} / 2`,
-                                    xDistance: xDistance * width,
-                                    yDistance: yDistance * height
-                                }
-                            )
-                        );
-                        startQueue.clear();
-                    }
-                }
-                // 广播蛇的位置
-                if(sendMessage.message == 'snakeRun'){
-                    broadcast(wsserver,JSON.stringify(
-                            {
-                                type:'snakeRun',
-                                message: sendMessage.position,
-                            }
-                        )
-                    );
-                }
-                // 判断食物被吃
-                if(sendMessage.message == 'eatFood'){
-                    foodIndex ++ ;
-                    if(foodIndex == 1){
-                        const xDistance = Math.floor(Math.random() * xAtoms);
-                        const yDistance = Math.floor(Math.random() * yAtoms);
-                        broadcast(wsserver,JSON.stringify(
-                                {
-                                    type:'eatFood',
-                                    xDistance: xDistance * width,
-                                    yDistance: yDistance * height
-                                }
-                            )
-                        );
-                    }
-                    if(foodIndex == 2){
-                        foodIndex = 0;
-                    }
-                }
+                console.log(sendMessage);
+                console.log(bundleFunction.filter(ele => ele.type == sendMessage.message)[0]);
+                bundleFunction.filter(ele => ele.type == sendMessage.message)[0].fn(sendMessage,broadcast);
             })
             conn.on('close',(code,reason) => {
                 if(code == 1001){
@@ -78,13 +118,14 @@ class wbServe{
             })
             conn.on('error',(code,reason) => {
             })
-        }).listen(8080) 
+        }).listen(8080);
+        // 广播消息
         function broadcast(server,msg){
             server.connections.forEach(conn => {
                 conn.sendText(msg)
             })
         }
-        console.log('服务器开启');
+        console.log(`服务器开启，地址是：${this.port}`);
     }
 }
 module.exports = {
